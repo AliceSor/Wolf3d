a401063027f0e03ebd0822b1ceb90676d6917089
asoroka <asoroka@student.unit.ua>
2017-08-15 20:04:06 +0300

    init

---
+++ wolf3d.h
@@ -0,0 +1,18 @@
+//
+// Created by Alisa Soroka on 8/15/17.
+//
+
+#ifndef WOLF3D_H
+
+typedef struct s_player
+{
+ double x;
+ double y;
+ double fov;
+ double dir;
+ double height;
+} t_player;
+
+#define WOLF3D_H
+
+#endif
