80174824dbed5b49df5b4e4769886e66baaa03ff
asoroka <asoroka@student.unit.ua>
2017-10-13 18:26:50 +0300

    sketch

---
+++ main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asoroka <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/15 18:01:08 by asoroka           #+#    #+#             */
+/*   Updated: 2017/10/13 18:24:19 by asoroka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../wolf3d.h"
+
+int				main(int argc, char **argv)
+{
+	t_data		*data;
+
+	if (argc > 1 && argc < 3)
+	{
+		//init(data);
+		//
+		//argc = parseMap(data);
+		//
+		//if (argc == -1)
+		//{
+		//	errorMessage();
+		//	return ();
+		//}
+		//
+		//rayCast();
+		//
+		//mlx_hook(data->mlx->win, 2, 5, buttons, rt);
+		//mlx_hook(data->mlx->win, 17, 0, destroy, 0);
+		//mlx_loop(data->mlx->mlx);
+	}
+	//errorMessage();
+	return (0);
+}
--- wolf3d.h
+++ wolf3d.h
@@ -3,16 +3,35 @@
 //
 
 #ifndef WOLF3D_H
+#define WOLF3D_H
+
+typedef struct s_mlx
+{
+	void		*win;
+	void		*mlx;
+	void		*im;
+	int			*imdata;
+	int			ls;
+}				t_mlx;
 
 typedef struct s_player
 {
- double x;
- double y;
- double fov;
- double dir;
- double height;
-} t_player;
+ double			x;
+ double			y;
+ double			fov;
+ double			dir;
+ double			height;
+}				t_player;
 
-#define WOLF3D_H
+typedef struct s_data
+{
+	t_mlx		*mlx;
+	t_player	*player;
+	char		**map;
+}				t_data;
+
+t_mlx			*create_win(void);
+void			create_image(t_mlx *mlx);
+void			put_pixel(int i, int color, t_mlx *mlx);
 
 #endif
